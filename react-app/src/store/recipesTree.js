6b4783963c8373950f9dc202fb3223f4b4f04c1b
Jordan <jordansacct@gmail.com>
2021-10-20 11:00:12 -0700

    units store finished, untested

--- react-app/src/store/recipesTree.js
+++ react-app/src/store/recipesTree.js
@@ -3,7 +3,6 @@
 /* ----------------------------------------------------------------------- */
 
 
-
 /* ----------------------------------------------------------------------- */
 /* ----------------------------Action Creators---------------------------- */
 /* ----------------------------------------------------------------------- */
@@ -20,16 +19,16 @@
 /* -----------------------Initial State & Reducer------------------------- */
 /* ----------------------------------------------------------------------- */
 
-const initialState = {}
+// const initialState = {}
 
-function recipesTreeReducer(state = initialState, action) {
-    let newState;
-    switch (action.type) {
-        case :
-            return
-        default:
-            return state;
-    }
-}
+// function recipesTreeReducer(state = initialState, action) {
+//     let newState;
+//     switch (action.type) {
+//         case :
+//             return
+//         default:
+//             return state;
+//     }
+// }
 
-export default recipesTreeReducer
+// export default recipesTreeReducer
--- react-app/src/store/units.js
+++ react-app/src/store/units.js
@@ -2,18 +2,64 @@
 /* -----------------------------Actions----------------------------------- */
 /* ----------------------------------------------------------------------- */
 
-
+const SET_UNITS = 'units/SET_UNITS';
+const DELETE_UNIT = 'units/DELETE_UNIT';
+const CREATE_UNIT = 'units/CREATE_UNIT'
 
 /* ----------------------------------------------------------------------- */
 /* ----------------------------Action Creators---------------------------- */
 /* ----------------------------------------------------------------------- */
 
+const setUnitsAction = (units) => {
+    return {
+        type: SET_UNITS,
+        units
+    };
+};
 
+
+const deleteUnitActions = (unit) => {
+    return {
+        type: DELETE_UNIT,
+        unit
+    };
+};
+
+const createUnitAction = (unit) => {
+    return {
+        type: CREATE_UNIT,
+        unit
+    };
+};
 
 /* ----------------------------------------------------------------------- */
 /* --------------------------------Thunks--------------------------------- */
 /* ----------------------------------------------------------------------- */
 
+export const setUnits = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/users/${userId}/units/`);
+    const units = await res.json();
+    dispatch(setUnitsAction(units));
+}
+
+export const createUnit = (unit) => async (dispatch) => {
+    const res = await fetch(`/api/units/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(unit)
+    });
+    const newUnit = res.json();
+    dispatch(createUnitAction(unit));
+}
+
+export const deleteUnit = (unit) => async (dispatch) => {
+    const res = await fetch(`/api/units/${unit.id}`, {
+        method: "DELETE"
+    });
+    dispatch(deleteUnitActions(unit));
+}
 
 
 /* ----------------------------------------------------------------------- */
@@ -25,11 +71,20 @@ const initialState = {}
 function unitsReducer(state = initialState, action) {
     let newState;
     switch (action.type) {
-        case :
-            return
+        case SET_UNITS:
+            newState = action.units;
+            return newState;
+        case CREATE_UNIT:
+            newState = {...state};
+            newState[action.unit.unit] = action.unit.id
+            return newState;
+        case DELETE_UNIT:
+            newState = {...state}
+            delete newState[action.unit.unit]
+            return newState
         default:
             return state;
     }
 }
 
-export default unitsReducer
+export default unitsReducer;
